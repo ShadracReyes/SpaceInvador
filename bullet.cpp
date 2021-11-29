fcf18f126feb7f75ce909c41eee7072f91080557
ShadracReyes <61811875+ShadracReyes@users.noreply.github.com>
2021-11-29 16:34:50 -0700

    Create bullet.cpp

---
+++ bullet.cpp
@@ -0,0 +1,15 @@
+class bullet {
+    protected:
+        int posX;
+        int posY;
+        int speedX;
+        int speedY;
+        
+
+
+        //SETTERS ---------------------------------------------
+
+        //GETTERS ---------------------------------------------
+};
+
+
